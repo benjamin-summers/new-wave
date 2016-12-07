9d98a8754df4ab5ced73f6762ecf006578fd3f7c
benjamin-summers <summerstyler2@gmail.com>
2016-12-06 21:40:17 -0500

    made changes to css

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ window.addEventListener('mouseup', function(event){
     box.style.display = 'none';
   }
 });
+
+window.addEventListener('touchstart', function(event){
+  var box = document.getElementById('box1');
+  if(event.target != box && event.target.parentNode != box) {
+    box.style.display = 'none';
+  }
+});
