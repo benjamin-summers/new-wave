5ace6530f0f8654570036524dbfe3d4268ea1538
benjamin-summers <summerstyler2@gmail.com>
2016-12-12 14:47:21 -0500

    switched to foundation

---
+++ NewWave/main.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 35.203897, lng: -85.1372127};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
